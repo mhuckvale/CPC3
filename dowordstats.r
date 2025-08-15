dc550aac94504740c5da3992f5a53a4b5ccb4c7e
Mark Huckvale <m.huckvale@ucl.ac.uk>
2025-08-15 15:20:58 +0100

    Add files via upload

--- dowordstats.r
+++ dowordstats.r
@@ -11,13 +11,14 @@ print(summary(df))
 dw=aggregate(cbind(HIT,LOGTRIPROB,LOGWFREQ,PWPOS,PCOUNT,SCOUNT,NCOUNT,NWORD)~WORD,data=df,FUN=mean);
 print(summary(dw))
 
-feats=c("STOI","RMSE","CORR","SEVERITY")
+feats=c("STOI","RMSE","CORR")
 
 for (f in feats) {
 	cat(sprintf("\n=================================== %s\n",f));
 	flush.console();
-	corr=cor(df$HIT,df[,f])
-	mi=cmi.pw(df$HIT,df[,f]);
+	subset=seq(1,nrow(df),5)			# too many rows otherwise
+	corr=cor(df$HIT[subset],df[subset,f])
+	mi=cmi.pw(df$HIT[subset],df[subset,f]);
 	cat(sprintf("%s: r=%.3f bcmi=%.3f\n",f,corr,mi$bcmi));
 }
 
